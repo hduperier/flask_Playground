f002c602e026ec6ff0a16c20559a2fa8d91b2a85
hduperier <59696097+hduperier@users.noreply.github.com>
2023-01-17 21:50:03 -0500

    Update fiML.py

--- fiML.py
+++ fiML.py
@@ -1,20 +1,19 @@
+import pickle
 from flask import Flask, request, jsonify
-from sklearn.externals import joblib
 
 app = Flask(__name__)
 
-# load pre-trained model
-model = joblib.load("loan_default_model.pkl")
+# load the model from a file
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # get loan applicant information from the request
-    data = request.get_json(force=True)
-    prediction = model.predict_proba([data['credit_score'], data['income'], data['loan_amount'], data['loan_term']])[0]
+    # get the input data from the front-end
+    data = request.get_json()
 
-    # format the response
-    output = {'default_probability': prediction[1]}
-    return jsonify(output)
+    # make predictions using the model
+    predictions = model.predict(data)
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # return the predictions to the front-end
+    return jsonify(predictions)

c93012ec771c0574e12c6139fdcfb50365af156f
hduperier <59696097+hduperier@users.noreply.github.com>
2023-01-25 14:25:26 -0500

    save

--- flaskPlaygroundApp/src/App.jsx
+++ flaskPlaygroundApp/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import styles from './style';
 
-const App = () => (
-  <div classname="bg-primary w-full overflow-hidden">
-    <div>
-      
+const App = () => {
+    return (<div classname="bg-primary w-full overflow-hidden">
+      <div className={'${styles.paddingX} ${styles.flexCenter}'}>
+        < div className={'${styles.boxWidth}'}>
+          Navbar
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default App
